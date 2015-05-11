155fba5d0e8175d3cde64e0ebff4f14d440a8e43
Kaushik Gopal <code@kaush.co>
2015-05-11 12:16:49 -0700

    chore: Add LeakCanary

--- app/src/main/java/co/kaush/rem/MyApp.java
+++ app/src/main/java/co/kaush/rem/MyApp.java
@@ -1,49 +1,57 @@
 package co.kaush.rem;
 
 import android.app.Application;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 import dagger.ObjectGraph;
 import timber.log.Timber;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 public class MyApp
-    extends Application {
+      extends Application {
 
-  private static MyApp _instance;
-  private ObjectGraph _objectGraph;
+    private static MyApp _instance;
+    private ObjectGraph _objectGraph;
+    private RefWatcher _refWatcher;
 
-  public static MyApp get() {
-    return _instance;
-  }
+    public static MyApp get() {
+        return _instance;
+    }
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
+    public static RefWatcher getRefWatcher() {
+        return MyApp.get()._refWatcher;
+    }
 
-    _instance = (MyApp) getApplicationContext();
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
-    _objectGraph = ObjectGraph.create(Modules.list(this));
-    inject(this);
+        _instance = (MyApp) getApplicationContext();
 
-    Timber.plant(new CrashReportingTree());
+        _objectGraph = ObjectGraph.create(Modules.list(this));
+        inject(this);
 
-    _initializeDefaultFont();
+        Timber.plant(new CrashReportingTree());
 
-  }
+        _initializeDefaultFont();
 
-  public ObjectGraph getObjectGraph() {
-    return _objectGraph;
-  }
+        _refWatcher = LeakCanary.install(this);
+    }
 
-  public void inject(Object object) {
-    getObjectGraph().inject(object);
-  }
+    public ObjectGraph getObjectGraph() {
+        return _objectGraph;
+    }
 
-  private void _initializeDefaultFont() {
-    CalligraphyConfig.initDefault(//
-        new CalligraphyConfig.Builder()//
-            //.setDefaultFontPath("fonts/Actor-Regular.ttf")//
-            .setDefaultFontPath("fonts/SourceSansPro-Regular.ttf")//
-            .setFontAttrId(R.attr.fontPath)//
-            .build());
-  }
+    public void inject(Object object) {
+        getObjectGraph().inject(object);
+    }
+
+    private void _initializeDefaultFont() {
+        CalligraphyConfig.initDefault(//
+              new CalligraphyConfig.Builder()//
+                    //.setDefaultFontPath("fonts/Actor-Regular.ttf")//
+                    .setDefaultFontPath("fonts/SourceSansPro-Regular.ttf")//
+                    .setFontAttrId(R.attr.fontPath)//
+                    .build());
+    }
 }
--- app/src/main/java/co/kaush/rem/ui/BaseFragment.java
+++ app/src/main/java/co/kaush/rem/ui/BaseFragment.java
@@ -3,22 +3,24 @@ package co.kaush.rem.ui;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.View;
+import co.kaush.rem.MyApp;
+import com.squareup.leakcanary.RefWatcher;
 import rx.subscriptions.CompositeSubscription;
 
 public abstract class BaseFragment
-    extends Fragment {
+      extends Fragment {
 
-  protected CompositeSubscription subscriptions = new CompositeSubscription();
+    protected CompositeSubscription subscriptions = new CompositeSubscription();
 
-  @Override
-  public void onActivityCreated(Bundle savedInstanceState) {
-    super.onActivityCreated(savedInstanceState);
-    ((BaseActivity) getActivity()).inject(this);
-  }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ((BaseActivity) getActivity()).inject(this);
+    }
 
-  @Override
-  public void onViewCreated(View view, Bundle savedInstanceState) {
-    super.onViewCreated(view, savedInstanceState);
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     /*
     1.  it's necessary to add the initialization again, in cases where we hit the back button in frags
         onResumption the subscription might have been un subscribed
@@ -26,14 +28,21 @@ public abstract class BaseFragment
     2.  we place the subscription re-initialization logic here vs onCreateView because
         it's common to override onCreateView and not call super.
     */
-    if (subscriptions == null || subscriptions.isUnsubscribed()) {
-      subscriptions = new CompositeSubscription();
+        if (subscriptions == null || subscriptions.isUnsubscribed()) {
+            subscriptions = new CompositeSubscription();
+        }
     }
-  }
 
-  @Override
-  public void onDestroyView() {
-    super.onDestroyView();
-    subscriptions.unsubscribe();
-  }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        RefWatcher refWatcher = MyApp.getRefWatcher();
+        refWatcher.watch(this);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        subscriptions.unsubscribe();
+    }
 }
