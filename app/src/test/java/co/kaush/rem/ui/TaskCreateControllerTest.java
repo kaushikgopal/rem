b1ad4983d05528193a06d8e7efdd9fb74004af78
Kaushik Gopal <code@kaush.co>
2015-05-11 18:00:08 -0700

    fix: mock only the now() method of CoreDateUtils using Mockito's spy

--- app/src/test/java/co/kaush/rem/ui/TaskCreateControllerTest.java
+++ app/src/test/java/co/kaush/rem/ui/TaskCreateControllerTest.java
@@ -3,27 +3,27 @@ package co.kaush.rem.ui;
 import co.kaush.rem.util.CoreDateUtils;
 import hirondelle.date4j.DateTime;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class TaskCreateControllerTest {
 
     private TaskCreateController _controller;
-    private TaskCreateController.ITalkToTaskCreateScreen _talkToTCSMock;
-    private CoreDateUtils _coreDateUtils;
+    private static TaskCreateController.ITalkToTaskCreateScreen _talkToTCSMock;
+    private static CoreDateUtils _coreDateUtils;
 
-    @Before
-    public void setUp() throws Exception {
-        _talkToTCSMock = mock(TaskCreateController.ITalkToTaskCreateScreen.class);
-        _coreDateUtils = mock(CoreDateUtils.class);
-
+    @BeforeClass
+    public static void setup1Time() {
+        _coreDateUtils = spy(new CoreDateUtils());
         when(_coreDateUtils.now()).thenReturn(new DateTime(1979, 4, 3, 7, 12, 20, 0));
-        when(_coreDateUtils.format(anyString(), any(DateTime.class))).thenCallRealMethod();
+        _talkToTCSMock = mock(TaskCreateController.ITalkToTaskCreateScreen.class);
     }
 
     @Test
