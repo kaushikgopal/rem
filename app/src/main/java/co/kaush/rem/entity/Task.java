c1d166d96340ebbd0f040fc96bd496e60b677136
Kaushik Gopal <code@kaush.co>
2015-08-17 09:10:44 -0700

    feat: add fields for Task + builder

--- app/src/main/java/co/kaush/rem/entity/Task.java
+++ app/src/main/java/co/kaush/rem/entity/Task.java
@@ -1,10 +1,19 @@
 package co.kaush.rem.entity;
 
+import android.database.Cursor;
 import android.support.annotation.IntDef;
+import co.kaush.rem.db.DbUtils;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import rx.functions.Func1;
 
-public abstract class Task {
+import static com.squareup.sqlbrite.SqlBrite.Query;
+
+
+public class Task {
 
     @IntDef({ STATUS_SNOOZED, STATUS_COMPLETED, STATUS_DELETED })
     @Retention(RetentionPolicy.SOURCE)
@@ -23,5 +32,103 @@ public abstract class Task {
     public static final String SNOOZE_INTERVAL = "snooze_interval";
     public static final String SNOOZE_COUNT = "snooze_count";
 
+    public Long id;
+    public String description;
+    public Integer status;
+    public Date dueDate;
+    public Integer snoozeCount;
+    public Integer snoozeInterval; // in minutes
 
+    private Task(Long id,
+                 String description,
+                 Integer status,
+                 Date dueDate,
+                 Integer snoozeCount,
+                 Integer snoozeInterval) {
+        this.id = id;
+        this.description = description;
+        this.status = status;
+        this.dueDate = dueDate;
+        this.snoozeCount = snoozeCount;
+        this.snoozeInterval = snoozeInterval;
+    }
+
+    public static final Func1<Query, List<Task>> MAP = new Func1<Query, List<Task>>() {
+        @Override public List<Task> call(Query query) {
+            Cursor cursor = query.run();
+            try {
+                List<Task> values = new ArrayList<>(cursor.getCount());
+
+                while (cursor.moveToNext()) {
+                    long id = DbUtils.getLong(cursor, ID);
+                    String description = DbUtils.getString(cursor, DESCRIPTION);
+                    Date dueDate = DbUtils.getDate(cursor, DUE_DATE);
+                    int status = DbUtils.getInt(cursor, STATUS);
+                    int snoozeCount = DbUtils.getInt(cursor, SNOOZE_COUNT);
+                    int snoozeInterval = DbUtils.getInt(cursor, SNOOZE_INTERVAL);
+
+                    Task t = TaskBuilder.aTask()
+                          .withId(id)
+                          .withDescription(description)
+                          .withDueDate(dueDate)
+                          .withSnoozeCount(snoozeCount)
+                          .withSnoozeInterval(snoozeInterval)
+                          .withStatus(status)
+                          .build();
+
+                    values.add(t);
+                }
+                return values;
+            } finally {
+                cursor.close();
+            }
+        }
+    };
+
+    public static class TaskBuilder {
+        public Long id;
+        public String description;
+        public Integer status;
+        public Date dueDate;
+        public Integer snoozeCount;
+        public Integer snoozeInterval; // in minutes
+
+        private TaskBuilder() {}
+
+        public static TaskBuilder aTask() { return new TaskBuilder();}
+
+        public TaskBuilder withId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public TaskBuilder withDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public TaskBuilder withStatus(Integer status) {
+            this.status = status;
+            return this;
+        }
+
+        public TaskBuilder withDueDate(Date dueDate) {
+            this.dueDate = dueDate;
+            return this;
+        }
+
+        public TaskBuilder withSnoozeCount(Integer snoozeCount) {
+            this.snoozeCount = snoozeCount;
+            return this;
+        }
+
+        public TaskBuilder withSnoozeInterval(Integer snoozeInterval) {
+            this.snoozeInterval = snoozeInterval;
+            return this;
+        }
+
+        public Task build() {
+            return new Task(id, description, status, dueDate, snoozeCount, snoozeInterval);
+        }
+    }
 }
