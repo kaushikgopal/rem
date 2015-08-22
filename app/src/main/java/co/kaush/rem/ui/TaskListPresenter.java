d37816e02de02d8f1b85e3e87e9984f35af0bd3a
Kaushik Gopal <code@kaush.co>
2015-08-22 15:30:19 -0700

    feat + test: task list presenter should give color for due daytime

--- app/src/main/java/co/kaush/rem/ui/TaskListPresenter.java
+++ app/src/main/java/co/kaush/rem/ui/TaskListPresenter.java
@@ -1,10 +1,14 @@
 package co.kaush.rem.ui;
 
+import co.kaush.rem.R;
 import co.kaush.rem.entity.Task;
 import co.kaush.rem.util.CoreDateUtils;
 import hirondelle.date4j.DateTime;
 
-import static co.kaush.rem.util.CoreDateUtils.*;
+import static co.kaush.rem.util.CoreDateUtils.DUE_DATE_DAYTIME;
+import static co.kaush.rem.util.CoreDateUtils.DUE_DATE_DAYTIME_WITH_YEAR;
+import static co.kaush.rem.util.CoreDateUtils.format;
+import static co.kaush.rem.util.CoreDateUtils.getDateTimeFor;
 
 public class TaskListPresenter {
     private CoreDateUtils _coreDateUtils;
@@ -20,5 +24,13 @@ public class TaskListPresenter {
         } else {
             return format(DUE_DATE_DAYTIME_WITH_YEAR, dueDate);
         }
+    }
+
+    public int getDueDayTimeColorIdFor(Task task) {
+        if (task.isOverdue(_coreDateUtils)) {
+            return R.color.orange_1;
+        }
+
+        return R.color.gray_2;
     }
 }
--- app/src/test/java/co/kaush/rem/ui/TaskListPresenterTest.java
+++ app/src/test/java/co/kaush/rem/ui/TaskListPresenterTest.java
@@ -1,5 +1,6 @@
 package co.kaush.rem.ui;
 
+import co.kaush.rem.R;
 import co.kaush.rem.entity.Task;
 import co.kaush.rem.util.CoreDateUtils;
 import hirondelle.date4j.DateTime;
@@ -31,13 +32,30 @@ public class TaskListPresenterTest {
 
     @Test
     public void DueDayTimeText_ShouldShowYear_WhenYearNotCurrent() {
-        Task task = aTask().withDueDate(getDateFor(new DateTime(1985, 3, 20, 9, 0, 0, 0))).build();
-        assertThat(_presenter.getDueDateTextFor(task)).isEqualTo("[Wed] 9:00 AM [1985]");
 
-        task = aTask().withDueDate(getDateFor(new DateTime(2013, 3, 20, 9, 0, 0, 0))).build();
-        assertThat(_presenter.getDueDateTextFor(task)).isEqualTo("[Wed] 9:00 AM");
+        assertThat(_presenter.getDueDateTextFor(getTaskOverdue())).isEqualTo("[Wed] 9:00 AM [1985]");
+
+        Task task = getTaskDueToday();
+        assertThat(_presenter.getDueDateTextFor(task)).isEqualTo("[Sat] 11:12 PM");
 
         task = aTask().withDueDate(getDateFor(new DateTime(2015, 3, 20, 9, 0, 0, 0))).build();
         assertThat(_presenter.getDueDateTextFor(task)).isEqualTo("[Fri] 9:00 AM [2015]");
+    }
+
+    @Test
+    public void DueDateTimeColor_ShouldBeRed_WhenTaskIsOverdue() {
+        assertThat(_presenter.getDueDayTimeColorIdFor(getTaskOverdue())).isEqualTo(R.color.orange_1);
+        assertThat(_presenter.getDueDayTimeColorIdFor(getTaskDueToday())).isEqualTo(R.color.gray_2);
+    }
+
+    // -----------------------------------------------------------------------------------
+    // fixtures
+
+    private Task getTaskOverdue() {
+        return aTask().withDueDate(getDateFor(new DateTime(1985, 3, 20, 9, 0, 0, 0))).build();
+    }
+
+    private Task getTaskDueToday() {
+        return aTask().withDueDate(getDateFor(new DateTime(2013, 3, 23, 23, 12, 20, 0))).build();
     }
 }
